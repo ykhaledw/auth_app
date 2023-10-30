2861c62d24a9bdec81ba8dfc98a7a8fd9bb31858
ykhaledw <youssefwahdan12@gmail.com>
2023-10-30 04:17:56 +0200

    add user registration method to the cubit

--- lib/cubits/user_registration_cubit.dart
+++ lib/cubits/user_registration_cubit.dart
@@ -1,6 +1,21 @@
+import 'package:auth_mobile_app/constants.dart';
 import 'package:auth_mobile_app/cubits/user_registration_states.dart';
+import 'package:auth_mobile_app/models/user_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 
-class UserRegistrationCubit extends Cubit<UserRegistrationState>{
+class UserRegistrationCubit extends Cubit<UserRegistrationState> {
   UserRegistrationCubit() : super(UserRegistrationInitial());
+
+  addUser(UserModel user) async {
+    emit(UserRegistrationLoading());
+
+    try {
+      var usersBox = Hive.box(kUserBox);
+
+      await usersBox.add(user);
+    } catch (e) {
+      emit(UserRegistrationFailure(e.toString()));
+    }
+  }
 }
