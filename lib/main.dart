813409a5483d890dbab9980d309829f479ec2a6f
ykhaledw <youssefwahdan12@gmail.com>
2023-10-30 04:53:08 +0200

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:auth_mobile_app/constants.dart';
 import 'package:auth_mobile_app/cubits/user_registration_cubit.dart';
 import 'package:auth_mobile_app/models/user_model.dart';
+import 'package:auth_mobile_app/simple_bloc_observer.dart';
 import 'package:auth_mobile_app/views/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kUserBox);
   Hive.registerAdapter(UserModelAdapter());
@@ -20,7 +22,7 @@ class AuthApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context)=> UserRegistrationCubit()),
+        BlocProvider(create: (context) => UserRegistrationCubit()),
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
